c56d97219ee7ad097a4060cd704cc76d904d3b69
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-11-05 20:31:57 -0300

    Register screen

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,19 +1,74 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm';
+import validator from 'validator';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeError, setError } from '../../actions/ui';
+import { startRegisterWithEmailPasswordName } from '../../actions/auth';
 
 const RegisterScreen = () => {
+
+      const dispatch = useDispatch();
+
+      const { msgError } = useSelector(state => state.ui);
+      console.log(msgError);
+
+      const [formRegister, handleInputRegister] = useForm({
+            name: 'Santiago',
+            email: 'santiago@gmail.com',
+            password: '123456',
+            password2: '123456'
+      });
+
+      const { name, email, password, password2 } = formRegister
+
+      const handleRegister = (e) => {
+            e.preventDefault();
+
+            if (isFormValid()) {
+                  dispatch(startRegisterWithEmailPasswordName(email, password, name))
+            }
+      }
+
+      const isFormValid = () => {
+
+            if (name.trim().length === 0) {
+                  dispatch(setError('Name is require!'))
+                  return false;
+            } else if (!validator.isEmail(email)) {
+                  dispatch(setError('Not an email!'))
+                  return false;
+            } else if (password !== password2 || password < 5) {
+                  dispatch(setError('Password must be at least 6 characters!!'))
+                  return false;
+            }
+
+            dispatch(removeError())
+
+            return true;
+      }
+
       return (
             <>
-
                   <h3 className='auth__title'>Register</h3>
 
-                  <form>
+                  <form onSubmit={handleRegister}>
+
+                        {
+                              msgError &&
+                              <div className='auth__alert-error'>
+                                    {msgError}
+                              </div>
+                        }
+
                         <input
                               type="text"
                               placeholder='Name'
                               name='name'
                               className='auth__input'
                               autoComplete='off'
+                              value={name}
+                              onChange={handleInputRegister}
                         />
                         <input
                               type="text"
@@ -21,18 +76,24 @@ const RegisterScreen = () => {
                               name='email'
                               className='auth__input'
                               autoComplete='off'
+                              value={email}
+                              onChange={handleInputRegister}
                         />
                         <input
                               type="password"
                               placeholder='Password'
                               name='password'
                               className='auth__input'
+                              value={password}
+                              onChange={handleInputRegister}
                         />
                         <input
                               type="password"
                               placeholder='Confirm password'
                               name='password2'
                               className='auth__input'
+                              value={password2}
+                              onChange={handleInputRegister}
                         />
 
                         <button
@@ -46,12 +107,7 @@ const RegisterScreen = () => {
                         >
                               Already registered?
                         </Link>
-
-
-
-
                   </form>
-
             </>
       )
 }
